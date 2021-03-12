c185addc9b854ca776c5cb13add40d51c46be3e1
Nekodoll <37606008+Nekodoll@users.noreply.github.com>
2021-03-12 09:09:12 +0700

    Add files via upload

---
+++ data.php
@@ -0,0 +1,13 @@
+<?php
+
+/* cookie */
+
+$PHPSESSID = "PHPSESSID=316b53b7ae3292a49b07d73ae4a0bd59";
+$SesHashKey = "SesHashKey=fcem3pi394ofjei4";
+$SesToken = "SesToken=ses_id%3D1276%26ses_key%3Dfcem3pi394ofjei4";
+
+/* data roll */
+
+$token = "6f89c43be6257ecc0be75a3585bec1f5cb8e10b95a7390c3b590f62aa72aa1ca";
+
+?>
